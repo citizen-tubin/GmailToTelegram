18ddc5dd619ad9f4ca90e1475a3460a719bb84b9
citizen-tubin <guyf@duck.com>
2023-06-12 16:49:18 +0300

    design changes and code cleanup

--- Mail/mail.py
+++ Mail/mail.py
@@ -1,48 +1,97 @@
 import os.path
-import base64
 import ast
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import configparser
-from Mail.label import create_label_if_not_exist
 
 config = configparser.ConfigParser()
 config.read('.ini')
 
 scopes = ast.literal_eval(config.get('MAIL', 'SCOPES'))
 search_labels = ast.literal_eval(config.get('MAIL', 'SEARCH_LABELS'))
-label_to_create = ast.literal_eval(config.get('MAIL', 'LABEL_TO_CREATE'))
+scanned_message_label = ast.literal_eval(config.get('MAIL', 'SCANNED_MESSAGE_LABEL'))
 token_path = config.get('MAIL', 'TOKEN_PATH')
 credentials_path = config.get('MAIL', 'CREDENTIALS_PATH')
 is_mark_as_read_enabled = config.getboolean('MAIL', 'IS_MARK_AS_READ_ENABLED')
-is_add_scanned_label_enabled = config.getboolean('MAIL', 'IS_ADD_SCANNED_LABEL_ENABLED')
 
 
 class Mail:
     def __init__(self):
         self.creds = ""
-        self.__get_credentials_info()
-        self.service = build('gmail', 'v1', credentials=self.creds)
-        self.label = create_label_if_not_exist(self.service, label_to_create)
+        self.service = ""
+        self.labels_info = []
+        self.labels_names = []
+        self.__initialize()
 
-    def get_mails_with_credentials(self, filter_by):
+    def create_label_if_not_exist(self, label_to_add):
+
+        if label_to_add not in self.labels_names:
+            new_label = {
+                'labelListVisibility': 'labelShow',
+                'messageListVisibility': 'show',
+                'name': label_to_add
+            }
+            created_label = self.service.users().labels().create(userId='me', body=new_label).execute()
+            print(f"Label '{label_to_add}' created with ID: {created_label['id']}")
+            return created_label
+        else:
+            print(f"Label '{label_to_add}' already exists")
+            existing_label = next((obj for obj in self.labels_info['labels'] if obj['name'] == label_to_add), None)
+            return existing_label
+
+    def get_labels_names(self):
+        self.labels_info = self.service.users().labels().list(userId='me').execute()
+        self.labels_names = [label['name'] for label in self.labels_info['labels']]
+
+    def create_filters_by_label_string(self, names):
+        filter_criteria = {
+            'criteria': {
+                'query': ''
+            },
+            'action': {
+                'addLabelIds': [],
+                'removeLabelIds': [],
+                'markImportant': True,
+                'neverSpam': True
+            },
+            'actionName': 'CUSTOM',
+            'shouldArchive': False,
+            'hasUserLabel': False,
+            'category': 'CATEGORY_PERSONAL'
+        }
+
+        for name in names:
+            if name in self.labels_names:
+                continue
+            label_info = self.create_label_if_not_exist(name)
+            filter_criteria['action']['addLabelIds'] = label_info['id']
+            filter_criteria['criteria']['query'] = label_info['name']
+            try:
+                self.service.users().settings().filters().create(userId='me', body=filter_criteria).execute()
+                print(f'Filter {name} created successfully.')
+            except Exception as error:
+                print(f'An error occurred: {error}')
+                raise SystemExit
+
+    def get_mails_with_credentials(self):
         try:
-            messages_meta_data = self.__get_messages_meta_data(filter_by)
+            messages_meta_data = self.__get_messages_meta_data()
             if not messages_meta_data:
                 return []
 
             summarized_messages = []
             for message_meta_data in messages_meta_data:
                 message_info = self.service.users().messages().get(userId='me', id=message_meta_data['id']).execute()
-                message_summary = {item['name']: item['value'] for item in message_info['payload']['headers']}
-                result = self.__convert_to_summary_structure(message_meta_data['id'], filter_by, message_info['snippet'], message_summary)
-                self.__mark_message_as_processed(message_meta_data['id'])
+                message_headers = {item['name']: item['value'] for item in message_info['payload']['headers']}
+                result = self.__convert_to_summary_structure(message_info['snippet'], message_headers)
+                self.__mark_message_as_scanned(message_meta_data['id'])
                 summarized_messages.append(result)
             return summarized_messages
         except Exception as error:
             print(f'An error occurred: {error}')
+            raise SystemExit
 
     def __get_credentials_info(self):
         if os.path.exists(token_path):
@@ -64,9 +113,9 @@ class Mail:
             with open(token_path, 'w') as token:
                 token.write(self.creds.to_json())
 
-    def __get_messages_meta_data(self, filter_by):
+    def __get_messages_meta_data(self):
         label_ids = self.get_label_ids()
-        query = "is:unread {}".format(filter_by)
+        query = "is:unread -label:{}".format(scanned_message_label)
         results = self.service.users().messages().list(userId='me', labelIds=label_ids, q=query).execute()
         return results.get('messages', [])
 
@@ -75,21 +124,20 @@ class Mail:
         labels = self.service.users().labels().list(userId='me').execute().get('labels', [])
 
         if not labels or len(labels) == 0:
-            pass
+            return label_ids
         for label in labels:
             if label['name'] in search_labels:
                 label_ids.append(label['id'])
         return label_ids
 
-    def __convert_to_summary_structure(self, message_id, filter_by, snippet, message_header):
+    def __convert_to_summary_structure(self, snippet, message_header):
         return f"Subject:{message_header['Subject']}\n" \
             f"From: {message_header['From']} \n" \
-            f"Artist: {filter_by} \n" \
+            f"Artist: {''} \n" \
             f"Mail Summary: {snippet} \n" \
             f"Sent At: {message_header['Date']}"
 
-    def __mark_message_as_processed(self, message_id):
-        labels_to_add = []
+    def __mark_message_as_scanned(self, message_id):
         labels_to_remove = []
         body = {}
 
@@ -97,8 +145,14 @@ class Mail:
             labels_to_remove.append('UNREAD')
             body['removeLabelIds'] = labels_to_remove
 
-        if is_add_scanned_label_enabled:
-            body['addLabelIds'] = [self.label['id']]
+        body['addLabelIds'] = [scanned_message_label['id']]
 
         self.service.users().messages().modify(userId='me', id=message_id,
                                                body=body).execute()
+
+    def __initialize(self):
+        self.__get_credentials_info()
+        self.service = build('gmail', 'v1', credentials=self.creds)
+        self.get_labels_names()
+        self.create_label_if_not_exist(scanned_message_label)
+
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from Mail.filter import create_filters
 from Mail.mail import Mail
 from Messaging_app.messaging import Message
 import ast
@@ -9,26 +8,23 @@ import asyncio
 config = configparser.ConfigParser()
 config.read('.ini')
 
-search_in_message = ast.literal_eval(config.get('MAIL','SEARCH_IN_MESSAGE'))
-run_mails_to_whatsapp_job = config.getboolean('MISSION','RUN_MAILS_TO_WHATSAPP_JOB')
-is_create_new_filters_enabled = config.getboolean('MAIL.FILTERS','IS_CREATE_NEW_FILTERS_ENABLED')
-filters_to_create = ast.literal_eval(config.get('MAIL.FILTERS','FILTERS_MESSAGE_CONTAINING'))
+is_run_mails_to_whatsapp_job_enabled = config.getboolean('MISSION', 'IS_RUN_MAILS_TO_WHATSAPP_JOB_ENABLED')
+is_create_new_filters_enabled = config.getboolean('MAIL.FILTERS', 'IS_CREATE_NEW_FILTERS_ENABLED')
+filters_to_create = ast.literal_eval(config.get('MAIL.FILTERS', 'FILTERS_MESSAGE_CONTAINING'))
 
 
 async def main():
     mail = Mail()
     message = Message()
     if is_create_new_filters_enabled:
-        create_filters(mail.service,filters_to_create)
+        mail.create_filters_by_label_string(filters_to_create)
 
-    if run_mails_to_whatsapp_job:
-        summarized_mails = [mail.get_mails_with_credentials(item) for item in search_in_message]
+    if is_run_mails_to_whatsapp_job_enabled:
+        summarized_mails = mail.get_mails_with_credentials()
 
         if len(summarized_mails) > 0:
             await message.send(summarized_mails)
             print('All unread mails with provided labels were read.')
-
-
 
 if __name__ == '__main__':
     asyncio.run(main())
