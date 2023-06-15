7bcd251e4533eb2cc19682c20131393807ff8a7c
citizen-tubin <guyf@duck.com>
2023-06-15 09:34:55 +0300

    committing with error cannot close a running event loop

--- Messaging_app/messaging.py
+++ Messaging_app/messaging.py
@@ -1,18 +1,30 @@
 import configparser
 import requests
-from telegram import Bot
+from datetime import datetime
+from telegram import Bot, Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from Messaging_app.message_exception import MessageException
 
 config = configparser.ConfigParser()
 config.read('.ini')
 
 bot_token = config.get('TELEGRAM', 'TOKEN')
-
+poll_interval_in_hours = config.getint('TELEGRAM', 'POLL_INTERVAL_IN_HOURS')
 
 class Message:
     def __init__(self):
-        self.chat_id = -1
+        self.chat_id = None
+        self.last_token_refresh_datetime = None
         self.__initialize()
+
+    def is_refresh_token_required(self):
+        # Telegram requires the user to send a message to the bot every 24 hours in order to refresh the token.
+        # This app will send a refresh request in smaller intervals in order to keep this service running
+        if self.last_token_refresh_datetime is not None:
+            time_diff = datetime.now() - self.last_token_refresh_datetime
+            diff_in_hours = time_diff.total_seconds() / 3600
+            return diff_in_hours >= poll_interval_in_hours
+        return False
 
     async def send(self, messages):
         bot = Bot(token=bot_token)
@@ -32,10 +44,28 @@ class Message:
         chat_id = response['result'][0]['message']['chat']['id']
         return chat_id
 
+    async def error(self, update:Update, context: ContextTypes.DEFAULT_TYPE):
+        print(f'Update {update} caused error: {context.error}')
+
+    async def __handle_user_response(self, update: Update):
+        message_type = update.message.chat.type
+        text = update.message.text
+        self.last_token_refresh_datetime = datetime.now()
+        print(f'User ({update.message.chat.id}) in {message_type} sent message: "{text}". Token is refreshed.')
+
+    def __start_incoming_messages_handler_bot(self):
+        print("Starting incoming message bot...")
+        app = Application.builder().token(bot_token).build()
+        app.add_handler(MessageHandler(filters.TEXT, self.__handle_user_response))
+        app.add_error_handler(self.error)
+
+        print("Polling...")
+        app.run_polling(poll_interval=5)
 
     def __initialize(self):
         try:
             self.chat_id = self.__get_chat_id()
+            self.__start_incoming_messages_handler_bot()
         except Exception as error:
             print(error)
             raise MessageException("Failed to initialize Message app.")
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ is_run_mail_to_whatsapp_job_enabled = config.getboolean('MISSION', 'IS_RUN_MAIL_
 is_create_new_filters_enabled = config.getboolean('MAIL.FILTERS', 'IS_CREATE_NEW_FILTERS_ENABLED')
 labels_to_filter_by = ast.literal_eval(config.get('MAIL.FILTERS', 'LABELS_TO_FILTER_BY'))
 failure_sleeping_time_in_seconds = config.getint('SYSTEM', 'FAILURE_SLEEPING_TIME_IN_SECONDS')
-sleeping_time_in_minutes_before_rescanning = config.getint('SYSTEM', 'SLEEPING_TIME_IN_MINUTES_BEFORE_RESCANNING')
+sleeping_time_in_minutes_before_rescanning = 0.01 #config.getint('SYSTEM', 'SLEEPING_TIME_IN_MINUTES_BEFORE_RESCANNING')
 
 
 async def main():
@@ -37,11 +37,11 @@ async def main():
             print('System will retry in {} seconds'.format(failure_sleeping_time_in_seconds))
             time.sleep(failure_sleeping_time_in_seconds)
 
-    if is_create_new_filters_enabled:
-        mail.create_filters_by_label_name(labels_to_filter_by)
+    await refresh_mail_if_required(message)
 
     if is_run_mail_to_whatsapp_job_enabled:
         while True:
+            await refresh_mail_if_required(message)
             summarized_mail = mail.get_mail_with_credentials()
 
             if len(summarized_mail) > 0:
@@ -51,6 +51,9 @@ async def main():
             print('The inbox will be rescanned in {} minutes'.format(sleeping_time_in_minutes_before_rescanning))
             time.sleep(sleeping_time_in_minutes_before_rescanning*60)
 
+async def refresh_mail_if_required(message):
+    if message.is_refresh_token_required():
+        await message.send("To refresh the token, please reply with any message.")
 
 if __name__ == '__main__':
     asyncio.run(main())
